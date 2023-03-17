a1c05d07926496268e9bec77b01f029aa316494f
Jordan Irwin <antumdeluge@gmail.com>
2023-03-17 02:07:47 -0700

    Initial commit

---
+++ doc/sources.md
@@ -0,0 +1,8 @@
+
+
+### Bases
+
+- [Cabbit's original bases](https://opengameart.org/node/24944) ([CC0][lic.cc0])
+
+
+[lic.cc0]: licenses/CC0-1.0.txt
---
+++ main.js
@@ -0,0 +1,16 @@
+
+/****************************************************
+ * Copyright (C) 2023 - Jordan Irwin (AntumDeluge)  *
+ ****************************************************
+ * This software is licensed under the MIT license. *
+ * See: LICENSE.txt for details.                    *
+ ****************************************************/
+
+function main() {
+  console.log("this document is a work-in-progress");
+  const tmp = document.createElement("div");
+  tmp.innerText = "this document is a work-in-progress";
+  document.body.appendChild(tmp);
+}
+
+window.onload = main;
