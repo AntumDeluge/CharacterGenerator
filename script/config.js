0d00b12dd67cbcc557d6fc08d1c88bfc82e7e460
Jordan Irwin <antumdeluge@gmail.com>
2023-03-18 18:30:06 -0700

    Add info page about assets licensing

---
+++ script/config.js
@@ -0,0 +1,14 @@
+
+/****************************************************
+ * Copyright (C) 2023 - Jordan Irwin (AntumDeluge)  *
+ ****************************************************
+ * This software is licensed under the MIT license. *
+ * See: LICENSE.txt for details.                    *
+ ****************************************************/
+
+"use strict";
+
+
+export const config = {
+  "asset-info": "https://github.com/AntumDeluge/CharacterGenerator/blob/master/assets/README.md"
+};
--- script/nav.js
+++ script/nav.js
@@ -9,7 +9,12 @@
 "use strict";
 
 import { SpriteGenerator } from "./SpriteGenerator.js";
+import { config } from "./config.js";
 
+
+window.onNavButton = function(href="/") {
+  document.location.href = href;
+};
 
 window.onDownload = function() {
   // prepare image
@@ -18,4 +23,17 @@ window.onDownload = function() {
   anchor.download = "sprite.png";
   // simulate click
   anchor.click();
+}
+
+const formatLinks = function() {
+  for (const element of document.getElementsByClassName("asset-info")) {
+    if (element instanceof HTMLAnchorElement) {
+      // link to tag for release
+      element.href = config["asset-info"];
+    }
+  }
+}
+
+window.onload = function() {
+  formatLinks();
 }
