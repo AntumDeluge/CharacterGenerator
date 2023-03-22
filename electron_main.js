c2252b44dcd89ba524b352d558d36d3c1c97b78e
Jordan Irwin <antumdeluge@gmail.com>
2023-03-22 03:24:12 -0700

    Initialize Node.js

---
+++ electron_main.js
@@ -0,0 +1,9 @@
+
+/****************************************************
+ * Copyright (C) 2023 - Jordan Irwin (AntumDeluge)  *
+ ****************************************************
+ * This software is licensed under the MIT license. *
+ * See: LICENSE.txt for details.                    *
+ ****************************************************/
+
+"use strict";
